64d886e08a403f69cac17c4f40c9764d7cb8890a
Sam Korn <korn94sam@gmail.com>
2024-01-30 10:49:11 -0700

    cleanup thermocouple code, make it run properly on a Pi

--- software/thermocouple/thermocouple_over_serial.py
+++ software/thermocouple/thermocouple_over_serial.py
@@ -3,29 +3,23 @@ from time import sleep
 from datetime import datetime
 
 # Replace '/dev/tty.board_name' with the appropriate serial port name
-serial_port = '/dev/tty.usbmodem11101'
+serial_port = '/dev/tty.usbmodem1101'
 baud_rate = 115200  # Adjust as needed
 
-def read_line_from_serial(ser):
+def read_line_from_serial():
     # Read a line from the serial port
-    data = ser.readline().decode('utf-8').rstrip()
-    # print(data)
-    result = float(data)
-    return result
-
-try:
     with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
-        temp_data = read_line_from_serial(ser)
-except serial.SerialException as e:
-    print(f"Error: {e}")
+        data = ser.readline().decode('utf-8').rstrip()
+        print(data)
+        # data = data.split("//")[1]
+        if "\\" in data:
+             data = data.split("\\")[1]
+        # return data
+        result = float(data)
+        return result
 
-
+# Read a line f
 while True:
-    try:
-        with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
-            temp_data = read_line_from_serial(ser)
-    except serial.SerialException as e:
-        print(f"Error: {e}")
-
+    temp_data = read_line_from_serial()
     print(f"{ datetime.now().isoformat()} --- {temp_data:.3f} degF")
     sleep(1)
