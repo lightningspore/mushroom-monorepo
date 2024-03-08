ce9226fae30bf3f4a2ddbbc2d0c18f4e445c4aa1
Sam Korn <korn94sam@gmail.com>
2024-03-08 09:16:28 -0700

    get wifi working on THS sensor

---
+++ software/pico-firmware/README.md
@@ -0,0 +1,12 @@
+# pico firmware
+All of the firmware is written in CircuitPython
+
+
+## Circup
+Circup is a tool for adding and updating libraries to the CircuitPython device.
+
+
+```
+# Install circup + other dev tools
+pip install -r requirements-dev.txt
+```
--- software/pico-firmware/ths_sensor/code.py
+++ software/pico-firmware/ths_sensor/code.py
@@ -1,44 +1,29 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
+import json
+import os
+
+
 import board
 import busio
 import adafruit_ahtx0
 
+import wifi
+import board
+import digitalio
+
+from adafruit_httpserver import Server, Request, Response
+from prometheus_express import check_network, start_http_server, CollectorRegistry, Counter, Gauge, Router
+
+# local imports
+from wifisetup import server, wifi
 
 # create the I2C shared bus
 i2c = busio.I2C(board.GP5, board.GP4) 
 sensor = adafruit_ahtx0.AHTx0(i2c, address=56)
 
-
-# while True:
-#     print("Temperature: ", am.temperature)
-#     print("Humidity: ", am.relative_humidity)
-#     time.sleep(2)
-
-
-import os
-
-import socketpool
-import wifi
-
-from adafruit_httpserver import Server, Request, Response
-
-ssid = os.getenv("WIFI_SSID")
-password = os.getenv("WIFI_PASSWORD")
-
-print("Connecting to", ssid)
-wifi.radio.connect(ssid, password)
-print("Connected to", ssid)
-
-pool = socketpool.SocketPool(wifi.radio)
-server = Server(pool, "/static", debug=True)
-
-
-from prometheus_express import check_network, start_http_server, CollectorRegistry, Counter, Gauge, Router
-
 registry = CollectorRegistry(namespace="pico_dht20")
+
+
 
 humidity_g = Gauge(
     'humidity_gauge',
@@ -55,7 +40,7 @@ temp_g = Gauge(
 )
 
 tempf_g = Gauge(
-    'temp_gauge',
+    'tempf_gauge',
     'temp sensor gauge fahrenheight',
     labels=['ip'],
     registry=registry
@@ -82,11 +67,50 @@ def base(request: Request):
 def metrics(request: Request):
     print("metrics")
     temp_g.set(sensor.temperature)
+    tempf_g.set(sensor.temperature * 9 / 5 + 32)
     humidity_g.set(sensor.relative_humidity)
     metrics = "\n".join(registry.render())
     #print(metrics)
     return Response(request, metrics)
 
+@server.route("/metrics/json")
+def metrics(request: Request):
+    print("metrics json")
+    metrics = {"tempf": sensor.temperature * 9 / 5 + 32, "tempc": sensor.temperature, "humidity": sensor.relative_humidity}
+    #print(metrics)
+    return Response(request, json.dumps(metrics))
 
 
-server.serve_forever(str(wifi.radio.ipv4_address))
+
+
+def main():
+    led = digitalio.DigitalInOut(board.LED)
+    led.direction = digitalio.Direction.OUTPUT
+
+
+    i = 0
+    while i < 20:
+        time.sleep(0.1)
+        i += 1
+        led.value = i % 2
+
+    # server.serve_forever(str(wifi.radio.ipv4_address))
+    # server.serve_forever(port=80)
+    server.start(port=80)
+    while True:
+        led.value = not led.value
+        server.poll()
+        time.sleep(0.5)
+
+
+try:
+	main()
+except Exception as e:
+    import microcontroller
+    print("Error:\n", str(e))
+    print("Resetting microcontroller in 10 seconds")
+    time.sleep(10)
+    microcontroller.reset()
+
+
+
