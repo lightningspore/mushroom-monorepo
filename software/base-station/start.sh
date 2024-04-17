0a7e56bd48ec95c9ecb3319b68a4477c4bcb8466
Sam Korn <korn94sam@gmail.com>
2024-04-17 17:01:31 -0600

    use supervisor to manage applications

--- software/base-station/start.sh
+++ software/base-station/start.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-echo "Discovering sensors on the network..."
-python3 /app/discovery.py
-
-echo "Starting Alloy"
-nohup alloy run /app/alloy.config \
-  --server.http.listen-addr=0.0.0.0:12345 \
-  > /app/alloy.log &
 
 while : ; do
   echo "Still alive :) :( :| :D :P ;)"
+  echo "Discovering sensors on the network..."
   python3 /app/discovery.py
   sleep 300
 done
